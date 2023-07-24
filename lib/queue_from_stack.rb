3bd4088529944ec173dc02d7a5a34e6eb5499086
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-24 10:16:34 -0400

    commit tree a6a642428902567234570d46ad2e189a91ba6642

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,37 @@
 require_relative './stack'
 
 # your code here
+class MyQueue
+
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add(value)
+            # To add an element to the end of the queue, we first move all elements from s2 to s1,
+    # then push the new element to s1, and finally move back all elements from s1 to s2.
+       until @s2.empty?
+        @s1.push(@s2.pop)
+    end
+        
+    @s1.push(value)
+
+    until @s1.empty?
+        @s2.push(@s1.pop)
+    end
+  end
+
+  def remove
+    # To remove and return the first element in the queue, we simply pop from s2.
+    @s2.pop
+  end
+
+  def peek
+    # To peek at the first element in the queue without removing it, we simply peek at s2.
+    @s2.peek
+  end
+
+
+
+end
